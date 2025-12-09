59b120774b51a84b6921bf6df72c1bab409834b9
tamannashah18 <ts.work.1809@gmail.com>
2025-12-09 15:17:20 +0530

    scroll to top after navigate added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import ScrollToTop from './components/ScrollToTop';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import AboutPage from './pages/AboutPage';
@@ -9,6 +10,7 @@ import ContactPage from './pages/ContactPage';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
         <Header />
         <main className="flex-grow">
           <Routes>
